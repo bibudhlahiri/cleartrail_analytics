15146a8e62c609745e01bccb4fa1bc393d4d1f18
Bibudh Lahiri <bibudhlahiri@gmail.com>
2016-03-04 11:09:07 -0500

    Added classify_flows to analytics_for_end_of_event.R

--- analytics_for_end_of_event.R
+++ analytics_for_end_of_event.R
@@ -1,8 +1,9 @@
 library(data.table)
+library(rpart)
+library(e1071)
 
-
-#Assign numbers to events. Give an event number to each packet depending on its timestamp. Find the flows that coincide time-wise with ends of events and mark them 
-#as terminating flows. Then, do an analysis of lengths (in terms of number of packets) of flows that are terminating vs flows that are non-terminating.
+#Assign numbers to events. Give an event number to each packet depending on its timestamp. Find the flows that coincide time-wise with ends of events and mark the last flow among them 
+#as terminating flow. Then, do an analysis of lengths (in terms of number of packets) of flows that are terminating vs flows that are non-terminating.
   
 terminating_flows <- function()
 {
@@ -66,9 +67,85 @@ terminating_flows <- function()
   write.table(flow_summaries, filename, sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
   
   setkey(flow_summaries, terminating_flow)
-  length_check <- flow_summaries[, list(median_n_packets = median(n_packets), avg_n_packets = mean(n_packets)), by = terminating_flow]
+  length_check <- flow_summaries[, list(median_n_packets = median(n_packets), avg_n_packets = mean(n_packets)), by = terminating_flow] 
+  
+  #avg_n_packets is 6.889302 for non-terminating flows, 4.12676 for terminating flows. However, median n_packets is 2 for both.
+  
   #TODO: Fit a decision tree on flow_summaries to predict terminating_flow
   revised_packets
+}
+
+classify_flows <- function()
+{
+  filename <- "/Users/blahiri/cleartrail_osn/SET3/TC1/flow_summaries.csv"
+  flow_summaries <- fread(filename, header = TRUE, sep = ",", stringsAsFactors = FALSE, showProgress = TRUE, 
+                           colClasses = c("numeric", "numeric", "character", "numeric", "numeric", "numeric", "numeric", "character", 
+                                          "numeric", "numeric", "numeric", "numeric", "numeric"),
+                           data.table = TRUE)
+  flow_summaries$terminating_flow <- as.factor(flow_summaries$terminating_flow)
+  train = sample(1:nrow(flow_summaries), 0.7*nrow(flow_summaries))
+  test = (-train)
+  cat(paste("Size of training data = ", length(train), ", size of test data = ", (nrow(flow_summaries) - length(train)), "\n", sep = ""))
+  
+  training_data <- flow_summaries[train, ]
+  training_data <- create_bs_by_over_and_undersampling(training_data)
+  test_data <- flow_summaries[test, ]
+  
+  print(table(training_data$terminating_flow)) 
+  print(table(test_data$terminating_flow))
+  
+  #Note: Event cannot be kept as a predictor as it would not be available in real data
+  tune.out <- tune.rpart(terminating_flow ~ n_packets + factor(direction) + n_downstream_packets + n_upstream_packets + 
+                          + upstream_bytes + total_bytes + avg_bytes_per_packet + avg_upstream_bytes_per_packet + avg_downstream_bytes_per_packet, 
+                         data = training_data, minsplit = c(5, 10, 15, 20), maxdepth = seq(5, 30, 5))
+  print(summary(tune.out))
+   
+  bestmod <- tune.out$best.model
+  test_data[, predicted_terminating_flow := as.character(predict(bestmod, newdata = test_data, type = "class"))]
+  
+  prec_recall <- table(test_data[, terminating_flow], test_data[, predicted_terminating_flow], dnn = list('actual', 'predicted'))
+  print(prec_recall)
+  
+  #Measure overall accuracy
+  setkey(test_data, terminating_flow, predicted_terminating_flow)
+  cat(paste("Overall accuracy = ", nrow(test_data[(terminating_flow == predicted_terminating_flow),])/nrow(test_data), 
+            ", recall = ", prec_recall[2,2]/sum(prec_recall[2,]), 
+            ", precision = ", prec_recall[2,2]/sum(prec_recall[,2]), "\n\n", sep = "")) #0.79360465
+  #terminating_flow can be identified with recall of 0.6521 and precision of 0.1923
+  
+  filename <- "/Users/blahiri/cleartrail_osn/SET3/TC1/flow_summaries_with_predicted_terminating_flow.csv"
+  #Write the test data back with the predicted values of terminating_flow. No need to write the data points that come from training data because predicted_terminating_flow will be NA for them.
+  setkey(test_data, session_id, flow_id)
+  test_data <- test_data[order(session_id, flow_id),]
+  write.table(test_data, filename, sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
+  test_data
+}
+
+create_bs_by_over_and_undersampling <- function(df)
+{
+  set.seed(1)
+  n_df <- nrow(df)
+  size_each_part <- n_df/2
+
+  majority_set <- df[(terminating_flow == FALSE),]
+  n_majority <- nrow(majority_set)
+  cat(paste("n_majority = ", n_majority, ", n_df = ", n_df, ", size_each_part = ", size_each_part, "\n", sep = ""))
+  sample_majority_ind <- sample(1:n_majority, size_each_part, replace = FALSE)
+  sample_majority <- majority_set[sample_majority_ind, ]
+    
+  minority_set <- df[(terminating_flow == TRUE),]
+  n_minority <- nrow(minority_set)
+  rep_times <- size_each_part%/%nrow(minority_set)
+  oversampled_minority_set <- minority_set
+  for (i in 1:(rep_times - 1))
+  {
+    oversampled_minority_set <- rbindlist(list(oversampled_minority_set, minority_set))
+  }
+  rem_sample_id <- sample(1:n_minority, size_each_part%%nrow(minority_set), replace = FALSE)
+  rem_sample <- minority_set[rem_sample_id, ]
+  oversampled_minority_set <- rbindlist(list(oversampled_minority_set, rem_sample))
+
+  bal_df <- rbindlist(list(sample_majority, oversampled_minority_set))
 }
 
 lookup_event <- function(LocalTime, events)
@@ -99,9 +176,3 @@ get_downstream_bytes <- function(dt)
   downstream_packets <- dt[(Direction == "downstream")]
   sum(downstream_packets$Rx)
 }
-
-
-
-
-
-
