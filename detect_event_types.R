f9eff79f81ce044d0c8c42a2e6c8b9c32b19d351
Bibudh Lahiri <bibudhlahiri@gmail.com>
2016-03-11 15:32:43 -0500

    Added method classify_packets_deep_learning

--- detect_event_types.R
+++ detect_event_types.R
@@ -18,7 +18,6 @@ create_bs_by_over_and_undersampling <- function(df)
   classes <- unique(df$Event)
   n_classes <- length(classes)
   size_each_part <- round(n_df/n_classes)
-  cat(paste("n_classes = ", n_classes, ", size_each_part = ", size_each_part, "\n", sep = ""))
   bal_df <- data.table()
   setkey(df, Event)
   
@@ -29,16 +28,13 @@ create_bs_by_over_and_undersampling <- function(df)
      if (n_this_set >= size_each_part)
      {
        #undersample
-       cat(paste("classes[i] = ", classes[i], ", n_this_set = ", n_this_set, ", undersample\n", sep = ""))
        sample_ind <- sample(1:n_this_set, size_each_part, replace = FALSE)
        sample_from_this_set <- this_set[sample_ind, ]
        bal_df <- rbindlist(list(bal_df, sample_from_this_set))
      }
      else
      {
-       
        rep_times <- size_each_part%/%n_this_set
-       cat(paste("classes[i] = ", classes[i], ", n_this_set = ", n_this_set, ", rep_times = ", rep_times, ", oversample\n", sep = ""))
        oversampled_set <- this_set
        if (rep_times >= 2)
        {
@@ -85,6 +81,9 @@ prepare_data_for_detecting_event_types <- function(revised_pkt_data_file, events
   setkey(revised_packets, Event)
   revised_packets <- revised_packets[((nchar(Event) > 0) & (Event != "character(0)")),]
   
+  #Keep only one between Tx and Rx as one of them is always 0
+  revised_packets[, pkt_bytes := ifelse(Tx > 0, Tx, Rx)]
+  
   #Join the timestamp-related aggregated features with the timestamps in this data to introduce additional features.
   hidden_and_vis_states <- fread(hidden_and_vis_states_file, header = TRUE, sep = ",", stringsAsFactors = FALSE, showProgress = TRUE, 
                     colClasses = c("Date", "numeric", "numeric", "numeric", "numeric", "numeric", "character", "numeric", "numeric", 
@@ -98,9 +97,6 @@ prepare_data_for_detecting_event_types <- function(revised_pkt_data_file, events
     
   #Change the key back to session_id and LocalTime so that all packets for a session are printed together
   setkey(revised_packets, session_id, LocalTime)
-  
-  #Keep only one between Tx and Rx as one of them is always 0
-  revised_packets[, pkt_bytes := ifelse(Tx > 0, Tx, Rx)]
   
   #Drop columns that are not needed for modeling. We need to retain the timestamp in the test data so that we can group by it later to get the majority vote among labeled events for a timestamp.
    
@@ -128,6 +124,9 @@ classify_packets_random_forest <- function()
   training_data[(Event %in% c("User_Login", "User_mouse_drag_end", "User_mouse_wheel_down")), Event := "Other"]
   training_data$Event <- droplevels(training_data$Event)
   
+  #filename <- "/Users/blahiri/cleartrail_osn/SET3/TC1/training_data.csv"
+  #write.table(training_data, filename, sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
+  
   training_data <- create_bs_by_over_and_undersampling(training_data)
   
   test_data <- prepare_data_for_detecting_event_types("/Users/blahiri/cleartrail_osn/SET3/TC2/RevisedPacketData_23_Feb_2016_TC2.csv", 
@@ -135,7 +134,6 @@ classify_packets_random_forest <- function()
                                                       "/Users/blahiri/cleartrail_osn/SET3/TC2/hidden_and_vis_states_23_Feb_2016_Set_II.csv")
   levels(test_data$DomainName) <- levels(training_data$DomainName)
   levels(test_data$Direction) <- levels(training_data$Direction)
-  levels(test_data$Event) <- levels(training_data$Event)
   levels(test_data$majority_domain) <- levels(training_data$majority_domain)
   
   cat(paste("Size of training data = ", nrow(training_data), ", size of test data = ", nrow(test_data), "\n", sep = ""))
@@ -144,12 +142,12 @@ classify_packets_random_forest <- function()
   print(table(test_data$Event))
   
   cols <- c("LocalTime", "session_id")
+  n_features <- ncol(training_data) - length(cols) - 1
+  tune.out <- tune.randomForest(Event ~ ., data = training_data[, .SD, .SDcols = -cols], ntree = c(500, 1000), nodesize = seq(10, 30, 10), mtry = seq(floor(sqrt(n_features)), n_features, 2))
+  print(tune.out)
+  bestmod <- tune.out$best.model
   
-  #tune.out <- tune.randomForest(Event ~ ., data = training_data[, .SD, .SDcols = -cols], ntree = c(500, 1000), nodesize = seq(10, 30, 10))
-  #print(summary(tune.out))
-  #bestmod <- tune.out$best.model
-  
-  bestmod <- randomForest(Event ~ ., data = training_data[, .SD, .SDcols = -cols])
+  #bestmod <- randomForest(Event ~ ., data = training_data[, .SD, .SDcols = -cols], mtry = 9)
   
   impRF <- bestmod$importance
   impRF <- impRF[, "MeanDecreaseGini"]
@@ -164,9 +162,104 @@ classify_packets_random_forest <- function()
   #Measure overall accuracy
   setkey(test_data, Event, predicted_event)
   accuracy <- nrow(test_data[(Event == predicted_event),])/nrow(test_data)
-  cat(paste("Overall accuracy = ", accuracy, "\n\n", sep = ""))
+  cat(paste("Overall accuracy = ", accuracy, "\n\n", sep = "")) #0.583388: seems like we need better features: 
+  #currently none of the features look very strong as the maximum (normalized) variable importance is 11.2%
+  measure_precision_recall(prec_recall)
+  
+  #cols <- c("LocalTime", "session_id", "Event")
+  #tuneRF(training_data[, .SD, .SDcols = -cols], training_data$Event, stepFactor=1.5)
+  
   bestmod
 }
 
+classify_packets_deep_learning <- function()
+{
+  library(h2o)
+  localH2O = h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, Xmx = '2g')
+                    
+  training_data <- prepare_data_for_detecting_event_types("/Users/blahiri/cleartrail_osn/SET3/TC1/RevisedPacketData_23_Feb_2016_TC1.csv", 
+                                                          "/Users/blahiri/cleartrail_osn/SET3/TC1/23_Feb_2016_Set_I.csv",
+                                                          "/Users/blahiri/cleartrail_osn/SET3/TC1/hidden_and_vis_states_23_Feb_2016_Set_I.csv")
+                                                          
+  #Merge the minor categories of events in training data into one
+  setkey(training_data, Event)
+  training_data[(Event %in% c("User_Login", "User_mouse_drag_end", "User_mouse_wheel_down")), Event := "Other"]
+  training_data$Event <- droplevels(training_data$Event)
+  
+  #training_data <- create_bs_by_over_and_undersampling(training_data)
+  
+  test_data <- prepare_data_for_detecting_event_types("/Users/blahiri/cleartrail_osn/SET3/TC2/RevisedPacketData_23_Feb_2016_TC2.csv", 
+                                                      "/Users/blahiri/cleartrail_osn/SET3/TC2/23_Feb_2016_Set_II.csv",
+                                                      "/Users/blahiri/cleartrail_osn/SET3/TC2/hidden_and_vis_states_23_Feb_2016_Set_II.csv")
+  levels(test_data$DomainName) <- levels(training_data$DomainName)
+  levels(test_data$Direction) <- levels(training_data$Direction)
+  levels(test_data$majority_domain) <- levels(training_data$majority_domain)
+  
+  cat(paste("Size of training data = ", nrow(training_data), ", size of test data = ", nrow(test_data), "\n", sep = ""))
+  
+  print(table(training_data$Event)) 
+  print(table(test_data$Event))
+  
+  cols <- c("LocalTime", "session_id", "Event")
+  features <- names(training_data) 
+  features <- features[!(features %in% cols)]
+  
+  training_data_h2o <- as.h2o(training_data, destination_frame = 'training_data')
+  model <- h2o.deeplearning(x = features,  # column numbers for predictors
+                            y = "Event",   # column number for label
+                            training_frame = training_data_h2o, # data in H2O format
+                            activation = "TanhWithDropout", # or 'Tanh'
+                            input_dropout_ratio = 0.2, # % of inputs dropout
+                            hidden_dropout_ratios = c(0.5,0.5,0.5), # % for nodes dropout
+                            balance_classes = TRUE, 
+                            hidden = c(50,50,50), # three layers of 50 nodes
+                            epochs = 100) # max. no. of epochs
 
+  test_data_h2o <- as.h2o(test_data, destination_frame = 'test_data')                       
+  h2o_yhat_test <- h2o.predict(model, test_data_h2o, type = "class")
+  df_yhat_test <- as.data.frame(h2o_yhat_test)
+  #h2o.shutdown()
+  
+  test_data[, predicted_event := as.character(df_yhat_test$predict)]
+  
+  prec_recall <- table(test_data[, Event], test_data[, predicted_event], dnn = list('actual', 'predicted'))
+  print(prec_recall)
+  
+  #Measure overall accuracy
+  setkey(test_data, Event, predicted_event)
+  accuracy <- nrow(test_data[(Event == predicted_event),])/nrow(test_data)
+  cat(paste("Overall accuracy = ", accuracy, "\n\n", sep = "")) #0.583388: seems like we need better features: 
+  #currently none of the features look very strong as the maximum (normalized) variable importance is 11.2%
+  measure_precision_recall(prec_recall)
+}
+
+measure_precision_recall <- function(prec_recall)
+{
+  #Compute the micro-average recall values of the classes
+  
+  dt_prec_recall <- as.data.table(prec_recall)
+  setkey(dt_prec_recall, actual)
+  row_totals <- dt_prec_recall[, list(row_total = sum(N)), by = actual]
+  setkey(row_totals, actual)
+  for_recall <- dt_prec_recall[row_totals, nomatch = 0]
+  setkey(for_recall, actual, predicted)
+  for_recall <- for_recall[(actual == predicted),]
+  for_recall[, recall := N/row_total]
+  #for_recall <- for_recall[, .SD, .SDcols = c("actual", "recall")]
+  setnames(for_recall, "actual", "Event")
+  print(for_recall)
+  cat("\n")
+  
+  #Compute the micro-average precision values of the classes
+  
+  setkey(dt_prec_recall, predicted)
+  column_totals <- dt_prec_recall[, list(column_total = sum(N)), by = predicted]
+  setkey(column_totals, predicted)
+  for_precision <- dt_prec_recall[column_totals, nomatch = 0]
+  setkey(for_precision, actual, predicted)
+  for_precision <- for_precision[(actual == predicted),]
+  for_precision[, precision := N/column_total]
+  #for_precision <- for_precision[, .SD, .SDcols = c("predicted", "precision")] 
+  print(for_precision)
+}
 
